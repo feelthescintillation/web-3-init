edec7c352561f193b3d8b7c07089c0dddd0a3764
Sijeesh Joshi <feelthescintillation@gmail.com>
2021-09-05 14:06:57 -0700

    added contract code

--- index.js
+++ index.js
@@ -26,5 +26,24 @@ const web3 = new Web3(rpcURL);
 
 web3.eth.getBalance(ethAddr, (err, wei) => {
   balance = web3.utils.fromWei(wei, "ether");
-  console.log(err, balance);
+  console.log("ETH Balance", balance);
+});
+
+// Contract code
+
+const abi = constants.devsForRevolutionContract.ABI;
+const contractAddress = constants.devsForRevolutionContract.address;
+const contract = new web3.eth.Contract(abi, contractAddress);
+
+contract.methods.totalSupply().call((err, result) => {
+  console.log("Contract Token supply", result);
+});
+contract.methods.name().call((err, result) => {
+  console.log("Contract Name", result);
+});
+contract.methods.symbol().call((err, result) => {
+  console.log("Contract Symbol", result);
+});
+contract.methods.balanceOf(ethAddr).call((err, result) => {
+  console.log("Account Tokens for contract", result);
 });
